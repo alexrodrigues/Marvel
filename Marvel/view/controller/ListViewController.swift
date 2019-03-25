a23556b79ae851c31747a815201426e43f01c8be
Alex Rodrigues <alex.rodrigues.developer@gmail.com>
2019-03-25 17:34:19 -0300

    performing the pagination

--- Marvel/view/controller/ListViewController.swift
+++ Marvel/view/controller/ListViewController.swift
@@ -35,11 +35,11 @@ class ListViewController: UIViewController {
     }
 
     private func fetch(page: Int) {
-        CharactersService().fetch()
+        CharactersService().fetch(lastIndex: page)
             .observeOn(MainScheduler.instance)
             .subscribe(onNext: { [weak self] response in
                 guard let self = self else { return }
-                self.charactersArray = response
+                self.charactersArray.append(contentsOf: response)
                 self.activityIndicator.stopAnimating()
                 self.listTableView.isHidden = false
                 self.listTableView.reloadData()
@@ -75,6 +75,12 @@ extension ListViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 68.0
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if indexPath.row == charactersArray.count - 1 {
+            fetch(page: charactersArray.count + 1)
+        }
     }
 }
 
