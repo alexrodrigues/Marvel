d12ce070ceff15093257c90f2cdb6fcb7bf3fc02
Alex Rodrigues <alex.rodrigues.developer@gmail.com>
2019-03-28 04:48:39 -0300

    logos

---
+++ MarvelTests/CharacterFactorySpec.swift
@@ -0,0 +1,20 @@
+//
+//  CharacterFactorySpec.swift
+//  MarvelTests
+//
+//  Created by Alex Rodrigues on 28/03/19.
+//  Copyright © 2019 Alex Rodrigues. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import Marvel
+
+class CharacterFactorySpec: QuickSpec {
+
+    override func spec() {
+        describe("CharacterFactory Spec") {
+            
+        }
+    }
+}
