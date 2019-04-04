a5a4610a1837f17e67c671007f804d3cf17dc425
Alex Rodrigues <alex.rodrigues.developer@gmail.com>
2019-04-03 21:15:36 -0300

    removing farfetch from readme

---
+++ Marvel/viewmodel/DetailViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  DetailViewModel.swift
+//  Marvel
+//
+//  Created by Alex Rodrigues on 03/04/19.
+//  Copyright © 2019 Alex Rodrigues. All rights reserved.
+//
+
+import UIKit
+
+struct DetailViewModel {
+
+}
