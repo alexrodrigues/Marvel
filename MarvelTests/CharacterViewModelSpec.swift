7c0eb5ee2828166c5241ab3e4debbabc93d1650d
Alex Rodrigues <alex.rodrigues.developer@gmail.com>
2019-04-16 21:48:12 -0300

    first things first, fixing tests

--- MarvelTests/CharacterViewModelSpec.swift
+++ MarvelTests/CharacterViewModelSpec.swift
@@ -36,7 +36,7 @@ class CharacterViewModelSpec: QuickSpec {
             it("testing image", closure: {
                 expect(viewModel.path).to(equal("https://marvel-image.com/900"))
                 expect(viewModel.imageExtension).to(equal("jpg"))
-                expect(viewModel.profileImage).to(equal("https://marvel-image.com/900/portrait_xlarge.jpg"))
+                expect(viewModel.profileImageUrl?.absoluteString).to(equal("https://marvel-image.com/900/portrait_xlarge.jpg"))
             })
         }
     }
